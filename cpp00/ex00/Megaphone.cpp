823869cc7954d1bd831f4532ed8637d48f2bb07e
jimin <tesla52@naver.com>
2022-11-22 16:52:31 +0900

    Meagphone 완성

--- cpp00/ex00/Megaphone.cpp
+++ cpp00/ex00/Megaphone.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+static void	cout_string(char *arg) {
+	std::string str = arg;
+	for (unsigned int i = 0; i < str.size(); i++)
+		std::cout << (unsigned char) (toupper(str[i]));
+}
+
+int	main(int argc, char* argv[]) {
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
-	std::string str = argv[1];
-	for (unsigned int i = 0; i < str.size(); i++)
-		std::cout << (unsigned char) (toupper(str[i]));
+	for (int i = 1; i < argc; i++) {
+		cout_string(argv[i]);
+	}
+	std::cout << std::endl;
 	return 0;
 }
