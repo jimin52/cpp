555329bbc9b1e6d6af4e325b9c40d5320a4bd9f1
jimin <tesla52@naver.com>
2022-11-30 20:00:50 +0900

    unsigned int -> char

--- cpp00/ex00/Megaphone.cpp
+++ cpp00/ex00/Megaphone.cpp
@@ -3,7 +3,7 @@
 static void	cout_string(char *arg) {
 	std::string str = arg;
 	for (unsigned int i = 0; i < str.size(); i++)
-		std::cout << static_cast<unsigned int>(toupper(str[i]));
+		std::cout << static_cast<char>(toupper(str[i]));
 }
 
 int	main(int argc, char* argv[]) {
