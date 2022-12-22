420dc015ad07fe0f0cb308e463639b56e1fb1403
jhmin95 <tesla52@naver.com>
2022-12-22 09:13:30 +0900

    main 약간 수정

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -19,8 +19,8 @@ int main()
 		const Animal *meta = new Animal();
 		const Animal *j = new Dog();
 		const Animal *i = new Cat();
-		std::cout << j->getType() << " " << std::endl;
 		std::cout << i->getType() << " " << std::endl;
+		std::cout << j->getType() << " " << std::endl;
 		i->makeSound(); //will output the cat sound!
 		j->makeSound();
 		meta->makeSound();
